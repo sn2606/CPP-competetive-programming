5a2093b25c0fdf0ccf7a7a56c16608693c4e4fc9
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-02-02 22:15:37 +0530

    Medium problems

---
+++ Strings/leetcode_804_uniq_morse_code.cpp
@@ -0,0 +1,33 @@
+// International Morse Code defines a standard encoding where each letter 
+// is mapped to a series of dots and dashes, as follows: "a" maps to ".-", 
+// "b" maps to "-...", "c" maps to "-.-.", and so on.
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+// [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..", "--",
+// "-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+// Now, given a list of words, each word can be written as a concatenation of 
+// the Morse code of each letter. For example, "cab" can be written as "-.-..--...", 
+// (which is the concatenation "-.-." + ".-" + "-..."). 
+// We'll call such a concatenation, the transformation of a word.
+// Return the number of different transformations among all words we have.
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string> d = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        unordered_set<string> s;
+        for (auto w : words) {
+            string code;
+            for (auto c : w) code += d[c - 'a'];
+            s.insert(code);
+        }
+        return s.size();
+        
+    }
+};
