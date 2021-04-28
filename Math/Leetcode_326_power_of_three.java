7e4bbc859ea30aa463e9dd8a8add6a96ac14e13b
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-04-28 09:12:36 +0530

    Leetcode 326 power of three

---
+++ Math/Leetcode_326_power_of_three.java
@@ -0,0 +1,9 @@
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        return (n <= 0) ? false : (1162261467 % n == 0);
+    }
+}
