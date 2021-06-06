7f625c4df26b61834716690f452c5cae2912f9a1
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-06-06 18:58:07 +0530

    Reverse Bits

---
+++ Bit Manipulation/reverse_bits.cpp
@@ -0,0 +1,27 @@
+// Reverse bits in a number efficiently
+// O(log2(n)) time, O(1) space
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    
+    unsigned int n, ans = 0;
+    cin >> n;
+    
+    while(n){
+        ans <<= 1;
+        ans |= (n & 1);
+        n >>= 1;
+    }
+    
+    cout << ans;
+    
+    return 0;
+}
